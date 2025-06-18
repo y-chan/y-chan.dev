f012fdfaebab31532f18408c1bac54dc88ee1859
y-chan <y-chan@y-chan.dev>
2025-06-18 18:48:02 +0900

    update portfolio

---
+++ src/components/portfolioData.ts
@@ -0,0 +1,118 @@
+/* eslint-disable perfectionist/sort-objects */
+export const certificates = [
+  { 
+    name: "基本情報技術者", 
+    date: "2019年12月", 
+    issuer: "独立行政法人 情報処理推進機構（IPA）"
+  },
+  { 
+    name: "情報処理安全確保支援士(未登録)", 
+    date: "2022年6月", 
+    issuer: "独立行政法人 情報処理推進機構（IPA）"
+  },
+]
+
+export const techStack = {
+  Languages: [
+    { name: "HTML/CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
+    { name: "JavaScript/TypeScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" },
+    { name: "Python", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" },
+    { name: "C/C++", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" },
+    { name: "Rust", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rust/rust-original.svg" },
+    { name: "SQL", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" },
+  ],
+  "Frameworks & Libraries": [
+    { name: "React", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" },
+    { name: "Next.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" },
+    { name: "Vue.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg" },
+    { name: "Nuxt.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nuxtjs/nuxtjs-original.svg" },
+    { name: "Astro", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg" },
+    { name: "MUI", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg" },
+    { name: "Tailwind CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" },
+    { name: "Electron", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/electron/electron-original.svg" },
+    { name: "Node.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" },
+    { name: "FastAPI", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg" },
+    { name: "PyQt", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/qt/qt-original.svg" },
+    { name: "PyTorch", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg" },
+  ],
+  Others: [
+    { name: "Git/GitHub", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" },
+    { name: "Cloudflare", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cloudflare/cloudflare-original.svg" },
+    { name: "Docker", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" },
+    { name: "Figma", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" },
+  ],
+}
+
+export const timeline = [
+  {
+    date: "2019年4月",
+    title: "大阪電気通信大学高等学校 入学",
+    description: "電子工業科(後の工学科)に入り、そこまでOSSを使う程度しかできなかったプログラミングを本格的に始めました。"
+  },
+  {
+    date: "2019年8月",
+    title: "セキュリティ・キャンプ全国大会2019 参加/修了",
+    description: "セキュリティ・キャンプ全国大会2019のOS開発ゼミに参加し、修了しました。周りのつよつよにボコボコにされました。"
+  },
+  {
+    date: "2020年6月 - 2021年3月",
+    title: "SecHack365 2020 参加/修了",
+    description: "SecHack365 2020の開発駆動コース仲山ゼミに参加し、修了しました。"
+  },
+  {
+    date: "2021年3月",
+    title: "第3回 中高生情報学研究コンテスト 入選",
+    description: "SecHackの成果をポスターにまとめ、発表したものが入選しました。"
+  },
+  {
+    date: "2021年12月",
+    title: "VOICEVOX OSSレビュワー拝命",
+    description: "VOICEVOXのOSSレビュワーになりました。"
+  },
+  {
+    date: "2022年3月",
+    title: "第4回 中高生情報学研究コンテスト 入選",
+    description: "個人的に音声合成技術を研究したものが入選しました。"
+  },
+  {
+    date: "2022年4月",
+    title: "電気通信大学 入学",
+    description: "情報理工学域 II類 セキュリティ情報学プログラムに入学しました。"
+  },
+  {
+    date: "2022年6月",
+    title: "情報処理安全確保支援士試験 合格",
+    description: "受かりました。"
+  },
+  {
+    date: "2022年9月",
+    title: "SHAREVOX リリース、技育展 2022 AI/データサイエンス部門 最優秀賞",
+    description: "SHAREVOXをリリースし、それを技育展で発表したものが最優秀賞を受賞しました。"
+  },
+  {
+    date: "2022年9月",
+    title: "セキュリティ・ミニキャンプ in 山梨 2022 講師",
+    description: "「Pythonで簡単なブロックチェーンを作ってみよう！」という講義をさせていただきました。"
+  },
+  {
+    date: "2022年12月",
+    title: "VOICEVOX OSSメンテナー拝命",
+    description: "VOICEVOXのOSSメンテナーになりました。"
+  },
+  {
+    date: "2023年11月",
+    title: "セキュリティ・ミニキャンプ in 北海道 2023 講師",
+    description: "「Bitcoinの検証の仕組みを作ろう！」という講義をさせていただきました。"
+  },
+  {
+    date: "2023年11月",
+    title: "歌奏絆(かなで つぐ) 公開",
+    description: "大学のサークルで行っていた、オリジナルキャラクターを作成するプロジェクトを公開しました。"
+  },
+  {
+    date: "2024年1月",
+    title: "VOICEVOX ソング/ハミング機能 β版リリース",
+    description: "VOICEVOXの新規機能であるソング/ハミング機能の音声合成エンジンを開発しました。"
+  }
+]
+/* eslint-enable perfectionist/sort-objects */
